7f048ead3113852be30eb8f114868ef10e0983a9
Sujit <mnamesujit@gmail.com>
2024-03-29 18:21:53 +0530

    sub-navbar implementation

--- utils/fetWithHeaders.ts
+++ utils/fetWithHeaders.ts
@@ -3,7 +3,6 @@ import { getSession } from './sessionUtils';
 export const fetchWithHeaders = async (url: string) => {
   const session:any = getSession();
   let id = session.replace(/"|'/g, ' ');
-  console.log("calling api")
 
   const res = await fetch(url, {
     headers: {
