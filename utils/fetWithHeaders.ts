eccb62ce0cf0293661bc0cdb81b7e4b9b4ebca41
Sujit <mnamesujit@gmail.com>
2024-04-10 20:49:11 +0530

    feat: feedback page

--- utils/fetWithHeaders.ts
+++ utils/fetWithHeaders.ts
@@ -1,16 +1,17 @@
-import { getSession } from './sessionUtils';
+import { getSession } from "./sessionUtils";
 
 export const fetchWithHeaders = async (url: string) => {
-  const session:any = getSession();
-  let id = session.replace(/"|'/g, ' ');
+  const session: any = getSession();
+  let id = session.replace(/"|'/g, " ");
 
   const res = await fetch(url, {
     headers: {
-      'Authorization': `Bearer ${session}`,
-      'id': `${id}`,
-      'Content-Type': 'application/json',
+      Authorization: `Bearer ${session}`,
+      id: `${id}`,
+      "Content-Type": "application/json",
     },
   });
-
-  return await res.json();
+  if (!res.ok) throw new Error(`Api Error: $(res.status)`);
+  return res.json();
 };
+
