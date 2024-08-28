876d5929c36dce037dc88cdab9e45ad07f2cb777
sujit <sujit.shrc@gmail.com>
2024-08-28 23:02:34 +0530

    fix scroll to down button

--- components/custom/scrolldown.tsx
+++ components/custom/scrolldown.tsx
@@ -1,19 +1,41 @@
 "use client";
+import { useEffect, useState } from "react";
 import { ChevronDown } from "lucide-react";
 
 const ScrollDownButton = () => {
+  const [showButton, setShowButton] = useState(true);
+
   const scrollDown = () => {
     window.scrollTo({
       top: window.innerHeight,
       behavior: "smooth",
     });
+    setShowButton(false);
   };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setShowButton(true);
+      } else if (window.scrollY > 0 && showButton) {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [showButton]);
+
+  if (!showButton) return null;
 
   return (
     <button
       onClick={scrollDown}
-      className="fixed bottom-40 sm:bottom-14 left-1/2 transform -translate-x-1/2 
-                 bg-white bg-opacity-20 backdrop-blur-sm 
+      className="fixed bottom-40 sm:bottom-14 left-1/2 transform -translate-x-1/2 bg-white bg-opacity-20 backdrop-blur-sm 
                  rounded-full p-3 shadow-lg
                  transition-all duration-500 ease-in-out
                  hover:bg-opacity-30 hover:shadow-xl
